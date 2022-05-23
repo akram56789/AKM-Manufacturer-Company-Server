c1070c8716aa4853adf137e46f91fd412d09ca41
akram56789 <akramhossain56789@gmail>
2022-05-23 23:12:58 +0600

    my orders api done

--- index.js
+++ index.js
@@ -12,13 +12,13 @@ const app = express();
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ukprd.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
- 
-async function run(){
-    try{
+
+async function run() {
+    try {
         await client.connect();
         const productCollection = client.db('akm').collection('product');
         const purchasingCollection = client.db('akm').collection('purchasing')
-        
+
         app.get('/product', async (req, res) => {
             const query = {};
             const cursor = productCollection.find(query);
@@ -31,22 +31,32 @@ async function run(){
             const product = await productCollection.findOne(query)
             res.send(product)
         });
-        app.get('/purchasing', async(req, res) =>{
+        app.get('/purchasing', async (req, res) => {
             const purchase = req.query.purchase;
-            const query = {purchase: purchase};
+            const query = { purchase: purchase };
             const purchasings = await purchasingCollection.find(query).toArray();
             res.send(purchasings);
-          })
+        })
 
-          app.post('/purchasing', async (req, res) => {
+        app.post('/purchasing', async (req, res) => {
             const newProduct = req.body;
             const result = await purchasingCollection.insertOne(newProduct);
             res.send(result);
         });
+        app.get('/myorders', async (req, res) => {
+            const client = req.query.client
+            console.log(client);
+            const query = { client}
+            const cursor = purchasingCollection.find(query);
+            const purchasing = await cursor.toArray();
+            res.send(purchasing)
+
+        })
+
 
     }
 
-    finally{
+    finally {
 
     }
 }
